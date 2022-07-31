d4e5eff72760f824ec690fab370619bf8b8dd1cb
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2022-07-31 12:24:34 +0200

    Command model created in terminal app

--- terminal/models.py
+++ terminal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+class Command(models.Model):
+    name = models.CharField(max_length=20)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
