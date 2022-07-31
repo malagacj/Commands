6617c691884e155f7618b4b14e2585e51efafe01
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2022-07-31 12:25:09 +0200

    Command model added to admin

--- terminal/admin.py
+++ terminal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Command
 
-# Register your models here.
+@admin.register(Command)
+class CommandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
