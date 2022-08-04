23ed50b8eebce8fe2ec3e12e49a840fafa1b8a85
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2022-08-04 20:06:50 +0200

    urls, views and templates added for CommandDetailView

--- terminal/urls.py
+++ terminal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CommandListView
+from .views import CommandListView, CommandDetailView
 
 app_name = 'terminal'
 
 urlpatterns = [
     path('', CommandListView.as_view(), name='index'),
     path('command/', CommandListView.as_view(), name='command_list'),
+    path('command/<int:pk>', CommandDetailView.as_view(), name='command_detail'),
 ]
--- terminal/views.py
+++ terminal/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Command
 
 
 class CommandListView(ListView):
+    model = Command
+
+
+class CommandDetailView(DetailView):
     model = Command
