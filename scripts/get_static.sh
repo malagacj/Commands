64e870ff36fae9e0dde9cee702c48a4bbb8abefd
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2022-08-30 13:42:38 +0200

    Updating get_static.sh to work together with settings static

--- scripts/get_static.sh
+++ scripts/get_static.sh
@@ -3,43 +3,41 @@
 # Variables definition
 env="env"
 root_folder=$(dirname $(dirname $(realpath $0)))
+project_name=`basename $root_folder`
 
 cd $root_folder
 
-if [ ! -d static ]; then
-  mkdir static
+if [ ! -d $project_name/static ]; then
+  mkdir $project_name/static
 fi
 
-cd static
+cd $project_name/static
 
 
-# Bootstrap 5
+function get_static_zip {
+  filename=$1
+  url=$2
+
+  rm -rf $filename
+  
+  wget -O $filename.zip https://github.com/twbs/bootstrap/archive/v5.2.0.zip
+  unzip -d $filename $filename.zip; rm $filename.zip
+  old_filename=`ls $filename`
+  mv $filename/$old_filename/* $filename
+  rm -rf $filename/$old_filename
+}
+
+# Bootstrap5
 filename=Bootstrap5
-rm -rf $filename
+url=https://github.com/twbs/bootstrap/archive/v5.2.0.zip
+get_static_zip $filename $url
 
-wget -O $filename.zip https://github.com/twbs/bootstrap/archive/v5.2.0.zip
-unzip -d $filename $filename.zip; rm $filename.zip
-old_filename=`ls $filename`
-mv $filename/$old_filename/* $filename
-rm -rf $filename/$old_filename
-
-
-# Bootstrap 5
+# Bootstrap5-examples
 filename=Bootstrap5-examples
-rm -rf $filename
-
-wget -O $filename.zip https://github.com/twbs/bootstrap/releases/download/v5.2.0/bootstrap-5.2.0-examples.zip
-unzip -d $filename $filename.zip; rm $filename.zip
-old_filename=`ls $filename`
-mv $filename/$old_filename/* $filename
-rm -rf $filename/$old_filename
+url=https://github.com/twbs/bootstrap/releases/download/v5.2.0/bootstrap-5.2.0-examples.zip
+get_static_zip $filename $url
 
 # FontAwesome
 filename=fontawesome
-rm -rf $filename
-
-wget -O $filename.zip https://use.fontawesome.com/releases/v5.14.0/fontawesome-free-5.14.0-web.zip
-unzip -d $filename $filename.zip; rm $filename.zip
-old_filename=`ls $filename`
-mv $filename/$old_filename/* $filename
-rm -rf $filename/$old_filename
+url=https://use.fontawesome.com/releases/v5.14.0/fontawesome-free-5.14.0-web.zip
+get_static_zip $filename $url
