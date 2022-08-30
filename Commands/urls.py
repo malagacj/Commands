d229cda9ad6a4ab6aae46551afa59639e617e47b
Jose Froylan Malaga Cardoza <plokoon1987@gmail.com>
2022-08-30 13:44:25 +0200

    Setting root url to be terminal app url

--- Commands/urls.py
+++ Commands/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='terminal/'), name='root'),
     path('admin/', admin.site.urls),
     path('terminal/', include('terminal.urls', namespace='terminal')),
 ]
